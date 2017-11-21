0648a98433279281bf4de8a024ce1dbd7922d230
mrjimh <mr.jimh@gmail.com>
2017-11-21 17:29:47 +0100

    Add files via upload

---
+++ innlevering2/javascript/javascript.php
@@ -0,0 +1,23 @@
+<script>
+function registrerKlasseJs(form)
+{
+    /*var kode = document.forms["klasse"]["kode"].value;
+	var navn = document.forms["klasse"]["navn"].value;*/
+
+    /*if (kode == "" || navn=="")
+	{
+        alert("KLASSEKODE MANGLER");
+	    return false;
+	}*/
+
+
+	if klasse.kode.value=="")
+	{
+		        alert("KLASSEKODE MANGLER");
+				klasse.kode.focus();
+				return false;
+
+
+	}
+}
+</script>
